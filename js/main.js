74bde0382c4e06e1bd3fd0b6d7de2f0e2c3b6355
HeliosAiden <heliosaidenismyname@gmail.com>
2024-01-07 11:12:17 +0700

    WIP adding functionalities

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
+// Nav menu
+const navMenu = document.getElementById("nav-menu"),
+      navToggle = document.getElementById("nav-toggle"),
+      navClose = document.getElementById("nav-close")
+
+  if (navToggle) {
+    navToggle.addEventListener("click", () => {
+      navMenu.classList.add("show-menu")
+    })
+  }
+  if (navClose) {
+    navClose.addEventListener("click", () => {
+      navMenu.classList.remove("show-menu")
+    })
+  }
+
+const navLinks = document.querySelectorAll(".nav-link")
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu")
+  navMenu.classList.remove("show-menu")
+}
+navLinks.forEach(i => i.addEventListener("click", linkAction))
+
+const scrollHeader = () => {
+  const header = document.getElementById("header")
+  if (window.scrollY >= 80) {
+    header.classList.add("scroll-header")
+  } else {
+    header.classList.remove("scroll-header")
+  }
+}
+
+window.addEventListener("scroll", scrollHeader)
+
 var swiper = new Swiper(".testimonial-wrapper", {
   loop: "true",
   pagination: {
@@ -5,3 +39,19 @@ var swiper = new Swiper(".testimonial-wrapper", {
     clickable: true,
   },
 });
+
+const theme = document.querySelector("#theme-button")
+const themeModal = document.querySelector(".customize-theme")
+
+const openThemeModal = () => {
+  themeModal.style.display = 'grid'
+}
+
+const closeThemeModal = e => {
+  if (e.target.classList.contains('customize-theme')) {
+    themeModal.style.display = 'none'
+  }
+}
+
+theme.addEventListener("click", openThemeModal)
+themeModal.addEventListener("click", closeThemeModal)
