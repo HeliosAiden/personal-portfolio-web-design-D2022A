341928cfa0fa4316c299b1b7fa56db5fc05a40a4
HeliosAiden <heliosaidenismyname@gmail.com>
2024-01-21 00:32:57 +0700

    fix root

--- js/injectElements.js
+++ js/injectElements.js
@@ -1,9 +1,11 @@
+const root = 'personal-portfolio-web-design-D2022A'
+
 export const injectElement = (input) => {
   const {id, path, images = [], style = null} = input
   const element = document.getElementById(`${id}`)
   if (!element) return;
   try {
-    fetch(`./${path}`)
+    fetch(`${root}/${path}`)
       .then((res) => res.text())
       .then((data) => {
         element.innerHTML = data;
