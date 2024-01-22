e72ea6fa42c55efc2fe40d62b8a6af42f5303d32
HeliosAiden <heliosaidenismyname@gmail.com>
2024-01-22 23:55:21 +0700

    init router

---
+++ js/router.js
@@ -0,0 +1,23 @@
+const route = (event) => {
+  event = event || window.event;
+  event.preventDefault();
+  window.history.pushState({}, "", event.target.href);
+};
+
+const routes = {
+  404: "/pages/404.html",
+  "/": "/index.html",
+  "/about": "/pages/about.html",
+  "/contact": "/pages/contact.html",
+};
+
+const handleLocation = async () => {
+  const path = window.location.pathname;
+  const route = routes[path] || routes[404];
+  const html = await fetch(route).then((data) => data.text());
+  document.getElementById("main").innerHTML = html;
+};
+
+window.onpopstate = handleLocation;
+window.route = route;
+handleLocation();
