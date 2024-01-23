ea61d0d154fbdcdbe7e7dc4ba254aa81e31d479c
HeliosAiden <heliosaidenismyname@gmail.com>
2024-01-23 19:43:49 +0700

    tesst

--- js/components.js
+++ js/components.js
@@ -1,5 +1,12 @@
-import action from "./injectElements.js";
-import el from "/constants/components.js";
-el.map((i) => {
-  action(i);
-});
+import a from "./injectElements.js";
+// import c from "/constants/components.js";
+import b from "/constants/components.js";
+
+const rootDir = window.location.pathname
+if (rootDir == '/personal-portfolio-web-design-D2022A/') {
+  await import('./constants/components.js').then(i => {
+    console.log(i)
+  })
+}
+
+b.map((i) => a(i));
