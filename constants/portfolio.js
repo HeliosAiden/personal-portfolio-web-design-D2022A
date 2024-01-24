efeed5b98a8cbc486b4c90e16c471475ac86f708
HeliosAiden <heliosaidenismyname@gmail.com>
2024-01-24 19:48:46 +0700

    add lesson 5

--- constants/portfolio.js
+++ constants/portfolio.js
@@ -1,234 +1,264 @@
 const portfolios = [
-    {
-        data: '6',
-        iframe: 'posts/lesson_6/excercise_1/index.html',
-        image: {
-            src: 'images/6/1.png',
-            alt: 'lesson_6-excercise_1'
-        },
-        term: 'Buổi 6',
-        title: 'Bài 1 buổi 6'
+  {
+    data: "6",
+    iframe: "posts/lesson_6/excercise_4/index.html",
+    image: {
+      src: "images/6/4.png",
+      alt: "lesson_6-excercise_4",
     },
-    {
-        data: '6',
-        iframe: 'posts/lesson_6/excercise_2/index.html',
-        image: {
-            src: 'images/6/2.png',
-            alt: 'lesson_6-excercise_2'
-        },
-        term: 'Buổi 6',
-        title: 'Bài 2 buổi 6'
+    term: "Buổi 6",
+    title: "Bài 4 buổi 6",
+  },
+  {
+    data: "1",
+    iframe: "posts/lesson_1/excercise_1/index.html",
+    image: {
+      src: "images/1/1.png",
+      alt: "lesson_1-excercise_1",
     },
-    {
-        data: '6',
-        iframe: 'posts/lesson_6/excercise_3/index.html',
-        image: {
-            src: 'images/6/3.png',
-            alt: 'lesson_6-excercise_3'
-        },
-        term: 'Buổi 6',
-        title: 'Bài 3 buổi 6'
+    term: "Buổi 1",
+    title: "Bài 1 buổi 1",
+  },
+  {
+    data: "1",
+    iframe: "posts/lesson_1/excercise_2/index.html",
+    image: {
+      src: "images/1/2.png",
+      alt: "lesson_1-excercise_2",
     },
-    {
-        data: '6',
-        iframe: 'posts/lesson_6/excercise_4/index.html',
-        image: {
-            src: 'images/6/4.png',
-            alt: 'lesson_6-excercise_4'
-        },
-        term: 'Buổi 6',
-        title: 'Bài 4 buổi 6'
+    term: "Buổi 1",
+    title: "Bài 2 buổi 1",
+  },
+  {
+    data: "1",
+    iframe: "posts/lesson_1/excercise_3/index.html",
+    image: {
+      src: "images/1/3.png",
+      alt: "lesson_1-excercise_2.1",
     },
-    {
-        data: '1',
-        iframe: 'posts/lesson_1/excercise_1/index.html',
-        image: {
-            src: 'images/1/1.png',
-            alt: 'lesson_1-excercise_1'
-        },
-        term: 'Buổi 1',
-        title: 'Bài 1 buổi 1'
+    term: "Buổi 1",
+    title: "Bài 2.1 buổi 1",
+  },
+  {
+    data: "1",
+    iframe: "posts/lesson_1/excercise_4/index.html",
+    image: {
+      src: "images/1/4.png",
+      alt: "lesson_1-excercise_2.2",
     },
-    {
-        data: '1',
-        iframe: 'posts/lesson_1/excercise_2/index.html',
-        image: {
-            src: 'images/1/2.png',
-            alt: 'lesson_1-excercise_2'
-        },
-        term: 'Buổi 1',
-        title: 'Bài 2 buổi 1'
+    term: "Buổi 1",
+    title: "Bài 2.2 buổi 1",
+  },
+  {
+    data: "1",
+    iframe: "posts/lesson_1/excercise_5/index.html",
+    image: {
+      src: "images/1/5.png",
+      alt: "lesson_1-excercise_3",
     },
-    {
-        data: '1',
-        iframe: 'posts/lesson_1/excercise_3/index.html',
-        image: {
-            src: 'images/1/3.png',
-            alt: 'lesson_1-excercise_2.1'
-        },
-        term: 'Buổi 1',
-        title: 'Bài 2.1 buổi 1'
+    term: "Buổi 1",
+    title: "Bài 3 buổi 1",
+  },
+  {
+    data: "2",
+    iframe: "posts/lesson_2/excercise_1/index.html",
+    image: {
+      src: "images/2/1.png",
+      alt: "lesson_2-excercise_1",
     },
-    {
-        data: '1',
-        iframe: 'posts/lesson_1/excercise_4/index.html',
-        image: {
-            src: 'images/1/4.png',
-            alt: 'lesson_1-excercise_2.2'
-        },
-        term: 'Buổi 1',
-        title: 'Bài 2.2 buổi 1'
+    term: "Buổi 2",
+    title: "Bài 1 buổi 2",
+  },
+  {
+    data: "2",
+    iframe: "posts/lesson_2/excercise_2/index.html",
+    image: {
+      src: "images/2/2.png",
+      alt: "lesson_2-excercise_2.1",
     },
-    {
-        data: '1',
-        iframe: 'posts/lesson_1/excercise_5/index.html',
-        image: {
-            src: 'images/1/5.png',
-            alt: 'lesson_1-excercise_3'
-        },
-        term: 'Buổi 1',
-        title: 'Bài 3 buổi 1'
+    term: "Buổi 2",
+    title: "Bài 2.1 buổi 2",
+  },
+  {
+    data: "2",
+    iframe: "posts/lesson_2/excercise_3/index.html",
+    image: {
+      src: "images/2/3.png",
+      alt: "lesson_2-excercise_2.2",
     },
-    {
-        data: '2',
-        iframe: 'posts/lesson_2/excercise_1/index.html',
-        image: {
-            src: 'images/2/1.png',
-            alt: 'lesson_2-excercise_1'
-        },
-        term: 'Buổi 2',
-        title: 'Bài 1 buổi 2'
+    term: "Buổi 2",
+    title: "Bài 2.2 buổi 2",
+  },
+  {
+    data: "2",
+    iframe: "posts/lesson_2/excercise_4/index.html",
+    image: {
+      src: "images/2/4.png",
+      alt: "lesson_2-excercise_3.1",
     },
-    {
-        data: '2',
-        iframe: 'posts/lesson_2/excercise_2/index.html',
-        image: {
-            src: 'images/2/2.png',
-            alt: 'lesson_2-excercise_2.1'
-        },
-        term: 'Buổi 2',
-        title: 'Bài 2.1 buổi 2'
+    term: "Buổi 2",
+    title: "Bài 3.1 buổi 2",
+  },
+  {
+    data: "2",
+    iframe: "posts/lesson_2/excercise_5/index.html",
+    image: {
+      src: "images/2/5.png",
+      alt: "lesson_2-excercise_3.2",
     },
-    {
-        data: '2',
-        iframe: 'posts/lesson_2/excercise_3/index.html',
-        image: {
-            src: 'images/2/3.png',
-            alt: 'lesson_2-excercise_2.2'
-        },
-        term: 'Buổi 2',
-        title: 'Bài 2.2 buổi 2'
+    term: "Buổi 2",
+    title: "Bài 3.2 buổi 2",
+  },
+  {
+    data: "2",
+    iframe: "posts/lesson_2/excercise_6/index.html",
+    image: {
+      src: "images/2/6.png",
+      alt: "lesson_2-excercise_3.3",
     },
-    {
-        data: '2',
-        iframe: 'posts/lesson_2/excercise_4/index.html',
-        image: {
-            src: 'images/2/4.png',
-            alt: 'lesson_2-excercise_3.1'
-        },
-        term: 'Buổi 2',
-        title: 'Bài 3.1 buổi 2'
+    term: "Buổi 2",
+    title: "Bài 3.3 buổi 2",
+  },
+  {
+    data: "2",
+    iframe: "posts/lesson_2/excercise_7/index.html",
+    image: {
+      src: "images/2/7.png",
+      alt: "lesson_2-excercise_3.4",
     },
-    {
-        data: '2',
-        iframe: 'posts/lesson_2/excercise_5/index.html',
-        image: {
-            src: 'images/2/5.png',
-            alt: 'lesson_2-excercise_3.2'
-        },
-        term: 'Buổi 2',
-        title: 'Bài 3.2 buổi 2'
+    term: "Buổi 2",
+    title: "Bài 3.4 buổi 2",
+  },
+  {
+    data: "2",
+    iframe: "posts/lesson_2/excercise_8/index.html",
+    image: {
+      src: "images/2/8.png",
+      alt: "lesson_2-excercise_4",
     },
-    {
-        data: '2',
-        iframe: 'posts/lesson_2/excercise_6/index.html',
-        image: {
-            src: 'images/2/6.png',
-            alt: 'lesson_2-excercise_3.3'
-        },
-        term: 'Buổi 2',
-        title: 'Bài 3.3 buổi 2'
+    term: "Buổi 2",
+    title: "Bài 4 buổi 2",
+  },
+  {
+    data: "3",
+    iframe: "posts/lesson_3/excercise_1/index.html",
+    image: {
+      src: "images/3/1.png",
+      alt: "lesson_3-excercise_1",
     },
-    {
-        data: '2',
-        iframe: 'posts/lesson_2/excercise_7/index.html',
-        image: {
-            src: 'images/2/7.png',
-            alt: 'lesson_2-excercise_3.4'
-        },
-        term: 'Buổi 2',
-        title: 'Bài 3.4 buổi 2'
+    term: "Buổi 3",
+    title: "Bài 1 buổi 3",
+  },
+  {
+    data: "3",
+    iframe: "posts/lesson_3/excercise_2/index.html",
+    image: {
+      src: "images/3/2.png",
+      alt: "lesson_3-excercise_2",
     },
-    {
-        data: '2',
-        iframe: 'posts/lesson_2/excercise_8/index.html',
-        image: {
-            src: 'images/2/8.png',
-            alt: 'lesson_2-excercise_4'
-        },
-        term: 'Buổi 2',
-        title: 'Bài 4 buổi 2'
+    term: "Buổi 3",
+    title: "Bài 2 buổi 3",
+  },
+  {
+    data: "4",
+    iframe: "posts/lesson_4/excercise_1/index.html",
+    image: {
+      src: "images/4/1.png",
+      alt: "lesson_4-excercise_1",
     },
-    {
-        data: '3',
-        iframe: 'posts/lesson_3/excercise_1/index.html',
-        image: {
-            src: 'images/3/1.png',
-            alt: 'lesson_3-excercise_1'
-        },
-        term: 'Buổi 3',
-        title: 'Bài 1 buổi 3'
+    term: "Buổi 3",
+    title: "Bài 1 buổi 4",
+  },
+  {
+    data: "4",
+    iframe: "posts/lesson_4/excercise_2/index.html",
+    image: {
+      src: "images/4/2.png",
+      alt: "lesson_4-excercise_2",
     },
-    {
-        data: '3',
-        iframe: 'posts/lesson_3/excercise_2/index.html',
-        image: {
-            src: 'images/3/2.png',
-            alt: 'lesson_3-excercise_2'
-        },
-        term: 'Buổi 3',
-        title: 'Bài 2 buổi 3'
+    term: "Buổi 4",
+    title: "Bài 2 buổi 4",
+  },
+  {
+    data: "4",
+    iframe: "posts/lesson_4/excercise_3/index.html",
+    image: {
+      src: "images/4/3.png",
+      alt: "lesson_4-excercise_3",
     },
-    {
-        data: '4',
-        iframe: 'posts/lesson_4/excercise_1/index.html',
-        image: {
-            src: 'images/4/1.png',
-            alt: 'lesson_4-excercise_1'
-        },
-        term: 'Buổi 3',
-        title: 'Bài 1 buổi 4'
+    term: "Buổi 4",
+    title: "Bài 3 buổi 4",
+  },
+  {
+    data: "4",
+    iframe: "posts/lesson_4/excercise_4/index.html",
+    image: {
+      src: "images/4/4.png",
+      alt: "lesson_4-excercise_4",
     },
-    {
-        data: '4',
-        iframe: 'posts/lesson_4/excercise_2/index.html',
-        image: {
-            src: 'images/4/2.png',
-            alt: 'lesson_4-excercise_2'
-        },
-        term: 'Buổi 4',
-        title: 'Bài 2 buổi 4'
+    term: "Buổi 4",
+    title: "Bài 4 buổi 4",
+  },
+  {
+    data: "5",
+    iframe: "posts/lesson_5/excercise_1/index.html",
+    image: {
+      src: "images/5/1.png",
+      alt: "lesson_5-excercise_1",
     },
-    {
-        data: '4',
-        iframe: 'posts/lesson_4/excercise_3/index.html',
-        image: {
-            src: 'images/4/3.png',
-            alt: 'lesson_4-excercise_3'
-        },
-        term: 'Buổi 4',
-        title: 'Bài 3 buổi 4'
+    term: "Buổi 5",
+    title: "Bài 1 buổi 5",
+  },
+  {
+    data: "5",
+    iframe: "posts/lesson_5/excercise_2/index.html",
+    image: {
+      src: "images/5/2.png",
+      alt: "lesson_5-excercise_2",
     },
-    {
-        data: '4',
-        iframe: 'posts/lesson_4/excercise_4/index.html',
-        image: {
-            src: 'images/4/4.png',
-            alt: 'lesson_4-excercise_4'
-        },
-        term: 'Buổi 4',
-        title: 'Bài 4 buổi 4'
+    term: "Buổi 5",
+    title: "Bài 2 buổi 5",
+  },
+  {
+    data: "5",
+    iframe: "posts/lesson_5/excercise_3/index.html",
+    image: {
+      src: "images/5/3.png",
+      alt: "lesson_5-excercise_3",
     },
-]
+    term: "Buổi 5",
+    title: "Bài 3 buổi 5",
+  },
+  {
+    data: "6",
+    iframe: "posts/lesson_6/excercise_1/index.html",
+    image: {
+      src: "images/6/1.png",
+      alt: "lesson_6-excercise_1",
+    },
+    term: "Buổi 6",
+    title: "Bài 1 buổi 6",
+  },
+  {
+    data: "6",
+    iframe: "posts/lesson_6/excercise_2/index.html",
+    image: {
+      src: "images/6/2.png",
+      alt: "lesson_6-excercise_2",
+    },
+    term: "Buổi 6",
+    title: "Bài 2 buổi 6",
+  },
+  {
+    data: "6",
+    iframe: "posts/lesson_6/excercise_3/index.html",
+    image: {
+      src: "images/6/3.png",
+      alt: "lesson_6-excercise_3",
+    },
+    term: "Buổi 6",
+    title: "Bài 3 buổi 6",
+  },
+];
 
-export default portfolios
+export default portfolios;
